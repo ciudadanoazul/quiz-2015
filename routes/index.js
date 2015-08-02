ce5140c6387791160e5a87bc7b191f02af6a19ae
Paul Bowring <paul.bowring@ntlworld.com>
2015-08-02 09:21:40 +0100

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
-// GET página de autor. 
+// GET página de autor.
 router.get('/author', function(req, res) {
   res.render('author');
 });
