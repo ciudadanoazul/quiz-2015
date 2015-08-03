3129cee7093f91be24c930457f518b80749e5e55
Paul Bowring <paul.bowring@ntlworld.com>
2015-08-03 10:50:55 +0100

    Editar Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -32,8 +32,6 @@ exports.index = function(req, res) {
 
 		var search = '%' + req.query.search.replace(/\s/g,"%") + '%';
 
-		console.log('PARAMETRO SEARCH = ' + search);
-
 		models.Quiz.findAll(
 			{where: ["pregunta like ?", search],	// Filtrar
 			 order: [['pregunta', 'ASC']]}			// Ordenar
@@ -87,6 +85,33 @@ exports.create = function(req, res) {
         .save({fields: ["pregunta", "respuesta"]})
         .then( function(){ res.redirect('/quizes')})
       }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  );
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz     // save: guarda campos pregunta y respuesta en DB
+        .save( {fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes');});
+      }     // Redirección HTTP a lista de preguntas (URL relativo)
     }
   );
 };
