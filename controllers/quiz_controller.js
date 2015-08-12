f553f56fc66b3de3420ad6cc4e7b946cf6958df1
Paul Bowring <paul.bowring@ntlworld.com>
2015-08-12 12:45:03 +0100

    Crear Comentarios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,14 @@ var models = require('../models/models.js');
 
 // Autoload :id
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.findById(quizId).then(
+  models.Quiz.find({
+          where: {
+              id: Number(quizId)
+          },
+          include: [{
+              model: models.Comment
+          }]
+      }).then(
 
   // Comprueba que existe el quiz y lo guarda
 	// Si no existe echa un error
